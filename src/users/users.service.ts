e3510a04fe6d35bb58c32147c434dd18ca1757c8
Studart Erico <erico.studart@bcg.com>
2026-05-27 12:21:40 -0300

    feat: add update route

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class UsersService {
@@ -13,4 +14,23 @@ export class UsersService {
         const newUser = this.repo.create({ email, password });
         return this.repo.save(newUser);
     }
+
+    findOne(id: number) {
+        return this.repo.findOneBy({id});
+    };
+
+    find(email: string) {
+        return this.repo.find( { where: { email } });
+    };
+
+    async update(id: number, updateParams: Partial<User>) {
+        const user = await this.findOne(id);
+        if (!user) {
+            throw new Error('User not found');
+        };
+        Object.assign(user, updateParams);
+        return this.repo.save(user);
+    };
+
+    remove() {};
 }
