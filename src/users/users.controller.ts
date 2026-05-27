055b3608b030539e69d109a773026cdeb7d44661
Studart Erico <erico.studart@bcg.com>
2026-05-27 13:02:51 -0300

    feat: add update route to controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, Delete, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class UsersController {
@@ -29,5 +31,13 @@ export class UsersController {
     @Delete('/:id')
     deleteUser(@Param('id') id: string) {
         return this.userService.remove(parseInt(id));
+    }
+
+    @Patch('/:id')
+    async updateUser(
+        @Param('id') id: string, 
+        @Body() body: UpdateUserDto
+    ): Promise<User> {
+        return await this.userService.update(parseInt(id), body);
     }
 }
