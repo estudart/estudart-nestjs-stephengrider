6fc4fff2cace76c48540e779605e6c897acd2af8
Studart Erico <erico.studart@bcg.com>
2026-06-04 16:14:25 -0300

    feat: implement interceptor class

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+    UseInterceptors,
+    NestInterceptor,
+    ExecutionContext,
+    CallHandler,
+} from "@nestjs/common";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass } from "class-transformer";
+
+export class SerializeInterceptor implements NestInterceptor {
+
+    intercept(
+        context: ExecutionContext, 
+        handler: CallHandler<any>
+    ): Observable<any> {
+        // Run something before the request is handled
+        // by the request handler
+
+        return handler.handle().pipe(
+            map((data: any) => {
+                console.log('Im running before response is sent out', data);
+            })
+        )
+    }
+}
