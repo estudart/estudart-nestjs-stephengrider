7f9960558da21a53e0e1f683e28eed625fc6fc78
Studart Erico <erico.studart@bcg.com>
2026-06-06 18:54:18 -0300

    feat: serialize decorator

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -2,14 +2,22 @@ import {
     NestInterceptor,
     ExecutionContext,
     CallHandler,
+    UseInterceptors
 } from "@nestjs/common";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { plainToInstance } from "class-transformer";
 
+interface ClassConstructor {
+    new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+    return UseInterceptors(new SerializeInterceptor(dto));
+}
+
 export class SerializeInterceptor implements NestInterceptor {
     constructor(private dto: any) {}
-
     intercept(
         context: ExecutionContext, 
         handler: CallHandler<any>
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,15 +7,15 @@ import {
     Patch,
     Param,
     Query,
-    UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { User } from './user.entity';
-import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
     constructor(
@@ -30,7 +30,6 @@ export class UsersController {
         );
     }
 
-    @UseInterceptors(new SerializeInterceptor(UserDto))
     @Get('/:id')
     findUser(@Param('id') id: string) {
         return this.userService.findOne(parseInt(id));
