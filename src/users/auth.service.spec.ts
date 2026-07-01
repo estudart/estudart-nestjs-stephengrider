21e8a7b5209eab55899a3bf670a678ba20f9b34c
Studart Erico <erico.studart@bcg.com>
2026-07-01 15:07:50 -0300

    feat: add signup test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,29 +2,52 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { BadRequestException } from "@nestjs/common";
 
-let service: typeof AuthService;
+describe('AuthService', () => {
+    let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
-beforeEach(async () => {
-    // Create a fake copy of the UsersService
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => 
-            Promise.resolve({ id: 1, email, password } as User)
-    };
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                provide: UsersService,
-                useValue: fakeUsersService,
-            }
-        ],
-    }).compile();
+    beforeEach(async () => {
+        // Create a fake copy of the UsersService
+        fakeUsersService = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => 
+                Promise.resolve({ id: 1, email, password } as User)
+        };
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService,
+                }
+            ],
+        }).compile();
 
-    service = module.get(AuthService);
-})
+        service = module.get(AuthService);
+    })
 
-it('can create an instance of AuthService', async () => {
-    expect(service).toBeDefined();
+    it('can create an instance of AuthService', async () => {
+        expect(service).toBeDefined();
+    });
+
+    it('creates a new user with salted and hashed password', async () => {
+        const user = await service.signup('some@email.com', 'some_password');
+        expect(user.email).toEqual('some@email.com');
+        expect(user.password).not.toEqual('some_password');
+        const [salt, hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    });
+
+    it('throws an error if try to signup with already in use email', async () => {
+        fakeUsersService.find = () => Promise.resolve(
+            [{ id: 1, email: 'a', password: '1'} as User]
+        );
+        await expect(service.signup('asdas@asdas.com', 'asdas')).rejects.toThrow(
+            BadRequestException,
+        );
+    });
 });
+
