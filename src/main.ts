e66e11629f881bc2008898230e25badc3c87227c
Studart Erico <erico.studart@bcg.com>
2026-07-26 16:44:25 -0300

    fix: set validation pipe as global middleware

--- src/main.ts
+++ src/main.ts
@@ -10,11 +10,6 @@ async function bootstrap() {
       keys: ['asdasd'],
     }),
   );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
