2699a702351f22280344d68ed2963edb8c65250e
Studart Erico <erico.studart@bcg.com>
2026-07-26 18:01:39 -0300

    fix: remove commented block of code

--- src/app.module.ts
+++ src/app.module.ts
@@ -29,12 +29,6 @@ const cookieSession = require('cookie-session');
         };
       },
     })
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'db.sqlite',
-    //   entities: [User, Report],
-    //   synchronize: true, 
-    // })
   ],
   controllers: [AppController],
   providers: [
