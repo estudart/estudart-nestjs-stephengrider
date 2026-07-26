f1218df7d67ffe1b4b991b3bc0d6c0f6277e1b2e
Studart Erico <erico.studart@bcg.com>
2026-07-26 13:54:38 -0300

    feat: add signin test to users controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -32,7 +32,13 @@ describe('UsersController', () => {
 
     fakeAuthService  = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({
+          id: 1,
+          email,
+          password
+        } as User);
+      },
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -73,5 +79,15 @@ describe('UsersController', () => {
     await expect(controller.findUser('1')).rejects.toThrow(
       NotFoundException,
     );
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 };
+    const user = await controller.signin(
+      {email: 'this.is@email.com', password: 'test'},
+      session
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
