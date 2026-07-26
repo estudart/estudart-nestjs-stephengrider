623834e59178e5477ac33419baee943763f77db9
Studart Erico <erico.studart@bcg.com>
2026-07-26 16:33:22 -0300

    feat: add auth e2e test files

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-    keys: ['asdasd']
-  }))
+  app.use(
+    cookieSession({
+      keys: ['asdasd'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
