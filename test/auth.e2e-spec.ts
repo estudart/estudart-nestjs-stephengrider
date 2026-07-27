c95847e62f0c0d24c83c502156d6b661bf14adf6
Studart Erico <erico.studart@bcg.com>
2026-07-26 21:29:55 -0300

    feat: add signup and whoami test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,17 +16,34 @@ describe('Authentication System', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
+  it('handles a signup request', async () => {
     const email = 'test_100@email.com';
     return request(app.getHttpServer())
-        .post('/auth/signup')
-        .send({ email, password: 'this_is@password' })
-        .expect(201)
-        .then((res) => {
-            const { id, email } = res.body;
-            expect(id).toBeDefined;
-            expect(email).toEqual(email);
-        });
+      .post('/auth/signup')
+      .send({ email, password: 'this_is@password' })
+      .expect(201)
+      .then((res) => {
+          const { id, email } = res.body;
+          expect(id).toBeDefined;
+          expect(email).toEqual(email);
+      });
+  });
+
+  it('signup as a new user then get the currently logged user', async () => {
+    const email = 'test_100@email.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email,  password: 'this.is@password' })
+      .expect(201)
+    
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie!)
+      .expect(200)
+    
+    expect(body.email).toEqual(email);
   });
 
   afterEach(async () => {
